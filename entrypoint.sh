94d5f80df67e2c03dc849ca78ec3af2f5fa1eee2
Marc-Andre Bouchard <marc-andre.bouchard@mrnf.gouv.qc.ca>
2023-09-19 15:09:56 +0000

    modif pour avoir la branche

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,10 @@ set -ue
 RepositoryName="${INPUT_REPOSITORY_NAME}"
 AwsRegion="${INPUT_AWS_REGION}"
 CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
+Branche="${INPUT_BRANCH_NAME}"
 
 git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git remote add sync ${CodeCommitUrl}
-git push sync --mirror
+git push sync ${Branche} 
